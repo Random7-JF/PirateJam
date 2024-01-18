d7e3a919e3c12e5b027b54f22608fca2ef692076
Jon Fingas <jon.fingas@gmail.com>
2024-01-18 03:30:45 -0600

    Weed spawner work, added timer

--- harvest-havoc/scenes/weed/weed_spawner.gd
+++ harvest-havoc/scenes/weed/weed_spawner.gd
@@ -3,20 +3,24 @@ extends Node
 @export var max_weeds_to_spawn: int = 5
 @export var weed_scene: PackedScene
 @export var tilemap: TileMap
+@export var timer_start: float = 10.0
+
+@onready var timer: Timer = $Timer
 
 var weed_spawn_range: Vector2i
+const TILE_SIZE: int = 32
 
 func _ready():
 	if not weed_scene:
 		printerr("Missing weed_scene")
 		return
+
 	weed_spawn_range = get_spawn_range()
 	spawn_weeds(weed_spawn_range)
 	
 func get_spawn_range() -> Vector2i:
 	var rect = tilemap.get_used_rect()
-	var tile_size = tilemap.get_rendering_quadrant_size()
-	return Vector2(rect.end.x * tile_size, rect.end.y * tile_size)
+	return Vector2i(rect.end.x * TILE_SIZE, rect.end.y * TILE_SIZE)
 
 func get_spawn_location(spawn_range: Vector2i) -> Vector2i:
 	return Vector2i(randi_range(0,spawn_range.x),randi_range(0,spawn_range.y))
@@ -26,3 +30,9 @@ func spawn_weeds(spawn_range: Vector2):
 		var instance = weed_scene.instantiate()
 		instance.global_position = get_spawn_location(spawn_range)
 		add_child(instance)
+
+
+func _on_timer_timeout():
+	spawn_weeds(weed_spawn_range)
+	timer.wait_time = randf_range(0, timer_start)
+	timer.start()
