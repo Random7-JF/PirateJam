817234d84c69b014598d294859778013b37d0be1
Jon Fingas <jon.fingas@gmail.com>
2024-01-21 00:37:54 -0600

    weed cleanup

--- harvest-havoc/scenes/weed/weed.gd
+++ harvest-havoc/scenes/weed/weed.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @onready var sprite: AnimatedSprite2D = $Sprite
 
-@export var lifetime: int = 0: set = set_lifetime
-@export var level_up_time: int = 2: set = set_level_up_time
+@export var lifetime: int = 0
+@export var level_up_time: int = 2
 @export var weed_level: int = 1: set = set_level
 
 const WEED_MAX_LEVEL: int = 3
@@ -15,9 +15,6 @@ func _ready():
 	game_manager.connect("lifetime_tick", level_up)
 	Events.emit_signal("weed_spawned")
 	update_weed_sprite()
-
-func _on_timer_timeout():
-	pass # Replace with function body.
 
 func update_weed_sprite() -> void:
 	if weed_level == 1:
@@ -31,19 +28,13 @@ func set_level(new_level: int) -> void:
 	weed_level = new_level
 	update_weed_sprite()
 
-func set_lifetime(new_lifetime: int) -> void:
-	lifetime = new_lifetime
-
-func set_level_up_time(time: int) -> void:
-	level_up_time = time
-
 func level_up() -> void:
 	if  lifetime >= level_up_time and weed_level < WEED_MAX_LEVEL:
 		if randi_range(1,2) % 2 == 0:
 				set_level(weed_level + 1)
-				set_lifetime(0)
-				set_level_up_time(weed_level * level_up_time / 2)
+				lifetime = 0
+				level_up_time = (weed_level * level_up_time / 2)
 				Events.emit_signal("weed_grew")
 				print("Growing weed -> ", name, " from ", weed_level, " to ", weed_level +1)
 	else:
-		set_lifetime(lifetime + 1)
+		lifetime += 1
