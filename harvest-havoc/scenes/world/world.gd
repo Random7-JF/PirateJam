83b0fce4129fbb1a312cee45a6730447979c0117
Jon Fingas <jon.fingas@gmail.com>
2024-01-22 00:20:01 -0600

    Weed spawning, growing, harvesting

---
+++ harvest-havoc/scenes/world/world.gd
@@ -0,0 +1,6 @@
+extends Node2D
+"""
+This script is going to the world manager or game manager of the game.
+UI elements should ask this for info needed.
+In-game objects should report to this.
+"""
